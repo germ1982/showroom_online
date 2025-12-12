1adeb74ef5592b1b252fc8ce051ea8594f44b255
GERM <gaston.mena.1982@gmail.com>
2025-12-11 21:06:41 -0300

    MODIFICANDO EL USER

--- views/user/_form.php
+++ views/user/_form.php
@@ -6,12 +6,37 @@ use yii\widgets\ActiveForm;
 /* @var $model app\models\User */
 /* @var $form yii\widgets\ActiveForm */
 ?>
+<style>
+      .div-boton {
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+      }
 
-<div class="user-form">
+      .custom-box {
+            border-radius: 10px;
+            padding: 15px 40px 16px 40px;
+            box-shadow: 0 0 20px #00bfff;
+      }
+
+       @media (max-width: 768px) {
+            .centro-celu {
+                  padding: 0px 15px 0px 15px;
+            }
+       }
+</style>
+
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="row justify-content-center overlay-div centro-celu">
 
-    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+      <div class="col-md-4">
+
+      </div>
+
+      <div class="col-md-4 custom-box">
+
+          <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
@@ -21,13 +46,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'status')->textInput() ?>
 
-  
-	<?php if (!Yii::$app->request->isAjax){ ?>
+    	<?php if (!Yii::$app->request->isAjax){ ?>
 	  	<div class="form-group">
 	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
 	    </div>
 	<?php } ?>
 
+        </div>
+
+      <div class="col-md-4">
+
+      </div>
+
+
+  
+
+
     <?php ActiveForm::end(); ?>
     
-</div>
+
